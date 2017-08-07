8cead7fdfc8e4406fa411bf6ae725c18f8219928
Aruna Gamage <admgbit@gmail.com>
2017-08-07 12:30:16 +0530

    Create Shop UI ----- Insert --- Update --- Delete ---

---
+++ app/src/main/java/com/palvision/androidpalvisionapilaravel/shop/ShopInsertActivity.java
@@ -0,0 +1,15 @@
+package com.palvision.androidpalvisionapilaravel.shop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.palvision.androidpalvisionapilaravel.R;
+
+public class ShopInsertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_insert);
+    }
+}
